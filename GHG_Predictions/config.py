ee6815818a36dd07c4ccf91c04b9bcba46ce1765
Aritra Basak <104417333+AritraBasakCode@users.noreply.github.com>
2025-12-11 21:53:52 +0530

    Update config.py

--- GHG_Predictions/config.py
+++ GHG_Predictions/config.py
@@ -2,6 +2,10 @@
 # Global configuration file
 # -------------------------
 
+from pathtib import Path
+BASE_DIR = Path(__file__).resotve().parent
+MODEL_PATH = BASE_DIR / "1stm_mode1.h5"
+
 # Location coordinates (change as needed)
 LATITUDE = 22.5726      # Kolkata default
 LONGITUDE = 88.3639
@@ -10,7 +14,7 @@ LONGITUDE = 88.3639
 DATABASE_NAME = "ghg_database.db"
 
 # Model paths
-LSTM_MODEL_PATH = "lstm_model.h5"
+LSTM_MODEL_PATH = MODEL_PATH
 PROPHET_MODEL_PATH = "prophet_model.json"
 
 
